6c8be51377b9835d265c6fce4dea7eb6e764081c
Uzair Ahmed <essani.addy@gmail.com>
2024-11-09 21:48:51 +0500

    filesAll

--- src/app/Components/Header/header.tsx
+++ src/app/Components/Header/header.tsx
@@ -1,9 +1,19 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Header() {
+  // use state Button
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toogleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  // return
   return (
-    <div className="  block w-full h-auto bg-black text-white md:flex md:justify-between md:px-12 md:h-[76px] overflow-hidden ">
+    <div className="flex justify-between px-5 py-1 items-center md:items-center w-full h-auto bg-black text-white md:flex md:justify-between md:px-12 md:h-[76px] overflow-hidden ">
       <div className="block items-center md:flex text-center ">
         <div className="flex justify-center py-2">
           {/* logo picture */}
@@ -14,28 +24,51 @@ export default function Header() {
           <Link href={"/"}> Uzair Ahmed</Link>
         </div>
       </div>
-      {/* nav bar */}
-      <ul className="flex justify-center py-2 gap-5 md:gap-10 items-center px-1 ">
-        <Link href={"/"}>
-          <li className="listAll">Home</li>
-        </Link>
 
-        <Link href={"#About"}>
-          <li className="listAll">About</li>
-        </Link>
-        <Link href={"#Contact"}>
-          <li className="listAll">Contact</li>
-        </Link>
-        <Link
-          className="
+      <div>
+        {/* nav bar */}
+
+        <ul className="hidden md:flex md:gap-10 items-center ">
+          <Link href={"/"}>
+            <li className="listAll">Home</li>
+          </Link>
+
+          <Link href={"#About"}>
+            <li className="listAll">About</li>
+          </Link>
+          <Link href={"#Contact"}>
+            <li className="listAll">Contact</li>
+          </Link>
+          <Link
+            className="
         bg-custom-blue px-1 rounded-md
         "
-          href={"cv.pdf"}
-          target="black"
-        >
-          <li className="listAll"> DOWNLOAD CV</li>
-        </Link>
-      </ul>
+            href={"cv.pdf"}
+            target="black"
+          >
+            <li className="listAll"> DOWNLOAD CV</li>
+          </Link>
+        </ul>
+
+        {/* small navbar */}
+        <div className="md:hidden" onClick={toogleMenu}>
+          {isMenuOpen ? (
+            <AiOutlineClose size={30} />
+          ) : (
+            <GiHamburgerMenu size={30} />
+          )}
+        </div>
+        {isMenuOpen && (
+          <ul className="  bg-green-700 flex flex-col gap-4 mt-4 md:hidden">
+            <li>
+              <a href="/" onClick={toogleMenu}>
+                Home
+              </a>
+            </li>
+            <li>Contact</li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
